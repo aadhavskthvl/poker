c5d6ff9c4b84b4ac5875e045a3c5dea34e71c89f
Nishad <nishad.deokar1@gmail.com>
2024-04-14 10:42:18 +0100

    firebase init

--- poker-bot-main/macos/Flutter/GeneratedPluginRegistrant.swift
+++ poker-bot-main/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import cloud_firestore
 import firebase_core
 import flutter_js
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FlutterJsPlugin.register(with: registry.registrar(forPlugin: "FlutterJsPlugin"))
 }
